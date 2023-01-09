630c3d416305027a5942e134998d8bbfa6a101e6
Jose Argueta <92650152+PirrachoDev@users.noreply.github.com>
2023-01-09 17:46:12 +0000

    coded createBoard method

--- src/Board.js
+++ src/Board.js
@@ -30,11 +30,20 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  };
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +51,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++){
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -62,11 +78,12 @@ class Board extends Component {
     }
 
     // TODO: flip this cell and the cells around it
+    
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    //this.setState({board, hasWon});
   }
 
 
@@ -79,6 +96,21 @@ class Board extends Component {
     // TODO
 
     // make table board
+    return (
+      <table className="Board">
+        <tbody>
+          {
+            this.state.board.map(
+              y => <tr>
+                {y.map(
+                  x => <Cell isLit={x}/>
+                )}
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    )
 
     // TODO
   }
