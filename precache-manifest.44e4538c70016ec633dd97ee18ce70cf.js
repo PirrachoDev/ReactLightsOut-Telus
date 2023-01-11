a33a34a7894df96eb4bb79d7c02a1bb3bf00d848
Jose Argueta <92650152+PirrachoDev@users.noreply.github.com>
2023-01-11 16:50:42 +0000

    Updates

---
+++ precache-manifest.44e4538c70016ec633dd97ee18ce70cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "955b7e0941a1607f2de7",
+    "url": "/reactlightsout-telus/static/css/main.fc9131d8.chunk.css"
+  },
+  {
+    "revision": "955b7e0941a1607f2de7",
+    "url": "/reactlightsout-telus/static/js/main.955b7e09.chunk.js"
+  },
+  {
+    "revision": "2a4eb8a055b70f360807",
+    "url": "/reactlightsout-telus/static/js/1.2a4eb8a0.chunk.js"
+  },
+  {
+    "revision": "7676348aa02e11106981",
+    "url": "/reactlightsout-telus/static/js/runtime~main.7676348a.js"
+  },
+  {
+    "revision": "e194b9a780737260cf1954d52029a78a",
+    "url": "/reactlightsout-telus/index.html"
+  }
+];
