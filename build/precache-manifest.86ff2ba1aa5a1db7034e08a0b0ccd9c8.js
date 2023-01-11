311d166b0cfcbcb9e14bcdbd9b08abf4200bf31f
Jose Argueta <92650152+PirrachoDev@users.noreply.github.com>
2023-01-11 18:31:39 +0000

    gh-pages not workign

---
+++ build/precache-manifest.86ff2ba1aa5a1db7034e08a0b0ccd9c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2daabb6649b270c206b1",
+    "url": "/ReactLightsOut-Telus/static/css/main.fc9131d8.chunk.css"
+  },
+  {
+    "revision": "2daabb6649b270c206b1",
+    "url": "/ReactLightsOut-Telus/static/js/main.2daabb66.chunk.js"
+  },
+  {
+    "revision": "2a4eb8a055b70f360807",
+    "url": "/ReactLightsOut-Telus/static/js/1.2a4eb8a0.chunk.js"
+  },
+  {
+    "revision": "8550746c303a33d1fa79",
+    "url": "/ReactLightsOut-Telus/static/js/runtime~main.8550746c.js"
+  },
+  {
+    "revision": "7215177370b708a770b2935c38cb21cf",
+    "url": "/ReactLightsOut-Telus/index.html"
+  }
+];
